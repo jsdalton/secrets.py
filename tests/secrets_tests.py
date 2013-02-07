56e9aaf2385e015072973910600356cb32a80c7c
Jim Dalton <jim@ere.net>
2013-02-07 13:32:19 -0800

    New tests

--- tests/secrets_tests.py
+++ tests/secrets_tests.py
@@ -5,9 +5,9 @@ import hashlib
 import os
 import shutil
 import unittest
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, DES, Blowfish
 
-from secrets import FileCryptor, Cryptor
+from secrets.cryptors import FileCryptor, Cryptor
 from tests import TEST_FILES_DIR
 
 
@@ -23,29 +23,53 @@ class FileCryptorTest(unittest.TestCase):
             'encryption-test-result': os.path.join(TEST_FILES_DIR, "config.ini.test.encrypted"),
             'decryption-test-result': os.path.join(TEST_FILES_DIR, "config.ini.test.decrypted"),
         }
-        self.cryptor = FileCryptor(key=TEST_KEY)
+        self.initialize_cryptor()
 
     def tearDown(self):
         for f in self.filepath.keys():
             if "test" in f and os.path.exists(self.filepath[f]):
                 os.remove(self.filepath[f])
 
+    def initialize_cryptor(self):
+        self.cryptor = FileCryptor(key=TEST_KEY)
 
-class CombinedFileTest(FileCryptorTest):
     def test_encrypt_and_decrypt_are_reversible(self):
         # setup - create test file
         shutil.copyfile(self.filepath['unencrypted'], self.filepath['test'])
 
         # test encrypt
         self.cryptor.encrypt_file(self.filepath['test'],
-                                  self.filepath['encryption-test-result'])
+                                  self.filepath['encryption-test-result'],
+                                  verify=False)
 
         # assert is different from the original
         self.assertFalse(filecmp.cmp(self.filepath['encryption-test-result'],
                                      self.filepath['test']))
 
         self.cryptor.decrypt_file(self.filepath['encryption-test-result'],
-                                  self.filepath['test'])
+                                  self.filepath['test'],
+                                  verify=False)
+
+        # assert back to original
+        self.assertTrue(filecmp.cmp(self.filepath['test'],
+                                    self.filepath['unencrypted']),
+                                    "Files don't match!")
+
+    def test_encrypt_and_decrypt_verified_are_reversible(self):
+        # setup - create test file
+        shutil.copyfile(self.filepath['unencrypted'], self.filepath['test'])
+
+        # test encrypt
+        self.cryptor.encrypt_file(self.filepath['test'],
+                                  self.filepath['encryption-test-result'],
+                                  verify=True)
+
+        # assert is different from the original
+        self.assertFalse(filecmp.cmp(self.filepath['encryption-test-result'],
+                                     self.filepath['test']))
+
+        self.cryptor.decrypt_file(self.filepath['encryption-test-result'],
+                                  self.filepath['test'], verify=True)
 
         # assert back to original
         self.assertTrue(filecmp.cmp(self.filepath['test'],
@@ -53,8 +77,8 @@ class CombinedFileTest(FileCryptorTest):
                                     "Files don't match!")
 
 
-class DecryptFileTest(FileCryptorTest):
-    def test_should_create_new_file_with_decrypted_extension(self):
+
+    def test_decrypt_file_should_create_new_file_with_decrypted_extension(self):
         # setup - create test file
         self.cryptor.encrypt_file(self.filepath['unencrypted'], self.filepath['test'])
 
@@ -64,7 +88,7 @@ class DecryptFileTest(FileCryptorTest):
         # assert
         self.assertTrue(os.path.exists(self.filepath['decryption-test-result']), "Decrypted file not created")
 
-    def test_should_create_new_file_with_provided_output_filename(self):
+    def test_decrypt_file_should_create_new_file_with_provided_output_filename(self):
         # setup
         specified_output_file_path = os.path.join(TEST_FILES_DIR, "foobar.test")
         assert not os.path.exists(specified_output_file_path)
@@ -84,7 +108,7 @@ class DecryptFileTest(FileCryptorTest):
             if os.path.exists(specified_output_file_path):
                 os.remove(specified_output_file_path)
 
-    def test_should_remove_encrypted_extension_if_exists(self):
+    def test_decrypt_file_should_remove_encrypted_extension_if_exists(self):
         # setup - create test file
         self.cryptor.encrypt_file(self.filepath['unencrypted'], self.filepath['encryption-test-result'])
 
@@ -94,14 +118,14 @@ class DecryptFileTest(FileCryptorTest):
         # assert
         self.assertTrue(os.path.exists(self.filepath['test']), "Decrypted file '%s' not created" % self.filepath['test'])
 
-    def test_should_not_allow_input_and_output_to_match(self):
+    def test_decrypt_file_should_not_allow_input_and_output_to_match(self):
         # setup - create test file
         self.cryptor.encrypt_file(self.filepath['unencrypted'], self.filepath['test'])
 
         with self.assertRaises(AssertionError):
             self.cryptor.decrypt_file(self.filepath['test'], self.filepath['test'])
 
-    def test_should_not_overwrite_existing_file_if_overwrite_is_false(self):
+    def test_decrypt_file_should_not_overwrite_existing_file_if_overwrite_is_false(self):
         # setup - create test file
         self.cryptor.encrypt_file(self.filepath['unencrypted'], self.filepath['test'])
         shutil.copyfile(self.filepath['encrypted'], self.filepath['encryption-test-result'])
@@ -110,12 +134,12 @@ class DecryptFileTest(FileCryptorTest):
             # test
             self.cryptor.decrypt_file(self.filepath['test'], self.filepath['encryption-test-result'], overwrite_existing=False)
 
-    def test_should_handle_encrypted_file_where_line_break_is_in_initialization_vector(self):
+    def test_decrypt_file_should_handle_encrypted_file_where_line_break_is_in_initialization_vector(self):
         # setup
 
         # Inject an artifical iv with linebreaks in it
-        iv_with_line_break = "123\n1234\n2341234"
-        assert len(iv_with_line_break) == AES.block_size
+        iv_with_line_break = "123\n1234\n2341234\nt3jgeggggg\ng43g33g3g3af1"[:self.cryptor.block_size]
+
         random_generator = Mock()
         random_generator.read = Mock()
         random_generator.read.return_value = iv_with_line_break
@@ -133,25 +157,34 @@ class DecryptFileTest(FileCryptorTest):
             self.cryptor.random_generator = original_random_generator
 
 
-    def test_should_raise_decryption_failure_if_password_incorrect(self):
+    def test_decrypt_file_verified_should_raise_value_error_if_password_incorrect(self):
         # setup
         shutil.copyfile(self.filepath['encrypted'], self.filepath['test'])
-        self.cryptor.encrypt_file(self.filepath['test'])
+        self.cryptor.encrypt_file(self.filepath['test'], verify=True)
 
         with self.assertRaises(ValueError):
             # test
             self.cryptor.key = self.cryptor.hash_key("bad key!")
-            self.cryptor.decrypt_file(self.filepath['encryption-test-result'], self.filepath['decryption-test-result'])
+            self.cryptor.decrypt_file(self.filepath['encryption-test-result'], self.filepath['decryption-test-result'], verify=True)
 
-    def test_should_not_create_file_if_decryption_fails(self):
+    def test_decrypt_file_should_not_raise_value_error_if_password_incorrect(self):
         # setup
         shutil.copyfile(self.filepath['encrypted'], self.filepath['test'])
-        self.cryptor.encrypt_file(self.filepath['test'])
+        self.cryptor.encrypt_file(self.filepath['test'], verify=False)
+
+        # test doesn't raise Value Error
+        self.cryptor.key = self.cryptor.hash_key("bad key!")
+        self.cryptor.decrypt_file(self.filepath['encryption-test-result'], self.filepath['decryption-test-result'], verify=False)
+
+    def test_decrypt_file_should_not_create_file_if_decryption_fails(self):
+        # setup
+        shutil.copyfile(self.filepath['encrypted'], self.filepath['test'])
+        self.cryptor.encrypt_file(self.filepath['test'], verify=True)
 
         # test
         self.cryptor.key = self.cryptor.hash_key("bad key!")
         try:
-            self.cryptor.decrypt_file(self.filepath['encryption-test-result'], self.filepath['decryption-test-result'])
+            self.cryptor.decrypt_file(self.filepath['encryption-test-result'], self.filepath['decryption-test-result'], verify=True)
         except ValueError:
             pass
 
@@ -159,9 +192,7 @@ class DecryptFileTest(FileCryptorTest):
         self.assertFalse(os.path.exists(self.filepath['decryption-test-result']),
                          "File '%s' should not be created if decryption fails." % self.filepath['decryption-test-result'])
 
-
-class EncryptFileTest(FileCryptorTest):
-    def test_should_create_new_file_with_default_encrypted_extension(self):
+    def test_encrypt_file_should_create_new_file_with_default_encrypted_extension(self):
         # setup - create test file
         shutil.copyfile(self.filepath['unencrypted'], self.filepath['test'])
 
@@ -171,7 +202,7 @@ class EncryptFileTest(FileCryptorTest):
         # assert
         self.assertTrue(os.path.exists(self.filepath['encryption-test-result']), "Encrypted file not created")
 
-    def test_should_create_new_file_with_provided_output_filename(self):
+    def test_encrypt_file_should_create_new_file_with_provided_output_filename(self):
         # setup
         specified_output_filename = "foobar.encrypted"
         specified_output_file_path = os.path.join(TEST_FILES_DIR, specified_output_filename)
@@ -189,7 +220,7 @@ class EncryptFileTest(FileCryptorTest):
             if os.path.exists(specified_output_file_path):
                 os.remove(specified_output_file_path)
 
-    def test_should_not_allow_input_and_output_to_match(self):
+    def test_encrypt_file_should_not_allow_input_and_output_to_match(self):
         # setup
         shutil.copyfile(self.filepath['unencrypted'], self.filepath['test'])
 
@@ -197,7 +228,7 @@ class EncryptFileTest(FileCryptorTest):
         with self.assertRaises(AssertionError):
             self.cryptor.encrypt_file(self.filepath['test'], self.filepath['test'])
 
-    def test_should_not_overwrite_existing_file_if_overwrite_is_false(self):
+    def test_encrypt_file_should_not_overwrite_existing_file_if_overwrite_is_false(self):
         shutil.copyfile(self.filepath['unencrypted'], self.filepath['test'])
         shutil.copyfile(self.filepath['unencrypted'], self.filepath['decryption-test-result'])
 
@@ -206,36 +237,35 @@ class EncryptFileTest(FileCryptorTest):
             self.cryptor.encrypt_file(self.filepath['test'], self.filepath['decryption-test-result'], overwrite_existing=False)
 
 
-class VerifyFileTest(FileCryptorTest):
-    def test_should_return_true_if_key_and_file_are_valid(self):
+    def test_verify_file_should_return_true_if_key_and_file_are_valid(self):
         # setup - create test file
-        self.cryptor.encrypt_file(self.filepath['unencrypted'], self.filepath['test'])
+        self.cryptor.encrypt_file(self.filepath['unencrypted'], self.filepath['test'], verify=True)
 
         # test
-        result = self.cryptor.verify_key_for_file(self.filepath['test'])
+        result = self.cryptor.verify_file(self.filepath['test'])
 
         # assert
         self.assertTrue(result)
 
-    def test_should_return_false_if_key_is_wrong(self):
+    def test_verify_file_should_return_false_if_key_is_wrong(self):
         # setup - create test file
-        self.cryptor.encrypt_file(self.filepath['unencrypted'], self.filepath['test'])
+        self.cryptor.encrypt_file(self.filepath['unencrypted'], self.filepath['test'], verify=True)
 
         # test
         self.cryptor.key = self.cryptor.hash_key("bad key!")
-        result = self.cryptor.verify_key_for_file(self.filepath['test'])
+        result = self.cryptor.verify_file(self.filepath['test'])
 
         # assert
         self.assertFalse(result)
 
-    def test_should_return_false_if_file_is_corrupt(self):
+    def test_verify_file_should_return_false_if_file_is_corrupt(self):
         # setup - create test file
-        self.cryptor.encrypt_file(self.filepath['unencrypted'], self.filepath['test'])
+        self.cryptor.encrypt_file(self.filepath['unencrypted'], self.filepath['test'], verify=True)
         with open(self.filepath['test'], 'rb+') as test_file:
             test_file.write("Some junk!")
 
         # test
-        result = self.cryptor.verify_key_for_file(self.filepath['test'])
+        result = self.cryptor.verify_file(self.filepath['test'])
 
         # assert
         self.assertFalse(result)
@@ -258,37 +288,78 @@ class CryptorTest(unittest.TestCase):
         self.assertTrue(decrypted_value)
         self.assertEqual(value, decrypted_value)
 
+    def test_encrypt_verified_and_decrypt_verified_should_be_reversible(self):
+        # setup
+        value = "Roses are red\nViolets are blue"
+
+        # test
+        encrypted_value = self.cryptor.encrypt(value, verify=True)
+        decrypted_value = self.cryptor.decrypt(encrypted_value, verify=True)
+
+        # assert
+        self.assertTrue(encrypted_value)
+        self.assertTrue(decrypted_value)
+        self.assertEqual(value, decrypted_value)
+
     def test_decrypt_should_raise_value_error_if_bad_key(self):
         # setup
-        encrypted_value = self.cryptor.encrypt("You are my sunshine...")
+        encrypted_value = self.cryptor.encrypt("You are my sunshine...", verify=True)
 
         # test
         with self.assertRaises(ValueError):
             self.cryptor.key = self.cryptor.hash_key("bad key!")
-            self.cryptor.decrypt(encrypted_value)
-    
-    def test_verify_key_should_return_digest_size_if_key_and_msg_are_valid(self):
+            self.cryptor.decrypt(encrypted_value, verify=True)
+
+    def test_decrypt_should_not_raise_value_error_if_bad_key(self):
         # setup
-        digest_size = hmac.new("foo", digestmod=self.cryptor.digestmod).digest_size
         encrypted_value = self.cryptor.encrypt("You are my sunshine...")
 
+        # test should not raise value error
+        self.cryptor.key = self.cryptor.hash_key("bad key!")
+        self.cryptor.decrypt(encrypted_value)
+    
+    def test_verify_should_return_digest_size_if_key_and_msg_are_valid(self):
+        # setup
+        digest_size = hmac.new("foo", digestmod=self.cryptor.digestmod).digest_size
+        encrypted_value = self.cryptor.encrypt("You are my sunshine...", verify=True)
+
         # test
-        result = self.cryptor.verify_key(encrypted_value)
+        result = self.cryptor.verify(encrypted_value)
 
         # assert
         self.assertEqual(result, digest_size)
 
-    def test_verify_key_should_return_false_if_key_is_invalid(self):
+    def test_verify_should_return_false_if_key_is_invalid(self):
         # setup
-        encrypted_value = self.cryptor.encrypt("You are my sunshine...")
+        encrypted_value = self.cryptor.encrypt("You are my sunshine...", verify=True)
         self.cryptor.key = self.cryptor.hash_key("bad key!")
 
         # test
-        result = self.cryptor.verify_key(encrypted_value)
+        result = self.cryptor.verify(encrypted_value)
 
         # assert
         self.assertFalse(result)
 
-class Sha512Test(CryptorTest):
+class Sha512DigestModCryptorTest(CryptorTest):
     def setUp(self):
         self.cryptor = Cryptor(key=TEST_KEY, digestmod=hashlib.sha512)
+
+class Sha512DigestModFileCryptorTest(FileCryptorTest):
+    def initialize_cryptor(self):
+        self.cryptor = FileCryptor(key=TEST_KEY, digestmod=hashlib.sha512)
+
+class DESCipherTest(CryptorTest):
+    def setUp(self):
+        self.cryptor = Cryptor(key=TEST_KEY, cipher_cls=DES)
+
+class DESFileCipherTest(FileCryptorTest):
+    def initialize_cryptor(self):
+        self.cryptor = FileCryptor(key=TEST_KEY, cipher_cls=DES)
+
+class BlowfishCipherTest(CryptorTest):
+    def setUp(self):
+        self.cryptor = Cryptor(key=TEST_KEY, cipher_cls=Blowfish)
+
+class BlowfishFileCipherTest(FileCryptorTest):
+    def initialize_cryptor(self):
+        self.cryptor = FileCryptor(key=TEST_KEY, cipher_cls=Blowfish)
